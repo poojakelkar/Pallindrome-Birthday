4c22973ed1a0a62f5f893f86a1676f15a02a75a3
Pooja Kelkar <poojakelkar53@gmail.com>
2021-09-24 14:43:15 +0530

    done day month year

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ function pallindromeString(str) {
 }
 
 function convertToStr(date) {
-    var datestr = { day: "", month: "", year: "" };
+    var datestr = {
+        day: "",
+        month: "",
+        year: ""
+    };
     if (date.day < 10) {
         datestr.day = "0" + date.day;
     } else {
@@ -28,7 +32,7 @@ function convertToStr(date) {
 
 function getAllDateFormat(date) {
     var dateStr = convertToStr(date);
-    debugger;
+
     var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
     var mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
     var yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
@@ -40,7 +44,60 @@ function getAllDateFormat(date) {
     return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
 }
 
-function checkPallindromeForAllDares(date) {
+function isLeapYear(year) {
+    if (year % 400 === 0) {
+        return true;
+
+    }
+    if (year % 100 === 0) {
+        return false;
+    }
+    if (year % 4 === 0) {
+        return true;
+    }
+    return false
+}
+
+function getNextDate(date) {
+
+    var day = date.day + 1;
+    var month = date.month;
+    var year = date.year;
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (month == 2) {
+
+        if (isLeapYear(year)) {
+            if (day > 29) {
+                day = 1
+                month++
+            } else {
+                if (day > 28) {
+                    day = 1
+                    month++
+                }
+            }
+
+        }
+    }else {
+        if (day > daysInMonth[month - 1]) {
+            day = 1;
+            month++
+        }
+    }
+
+    if (month > 12) {
+        month = 1
+        year++
+    }
+    return {
+        day: day,
+        month: month,
+        year: year
+    }
+}
+
+function checkPallindromeForAllDates(date) {
     var listOfPallindrome = getAllDateFormat(date);
     var isPallindome = 0;
 
@@ -54,8 +111,8 @@ function checkPallindromeForAllDares(date) {
 }
 
 var date = {
-    day: 22,
-    month: 2,
-    year: 2022,
+    day: 31,
+    month: 12,
+    year: 2021,
 };
-console.log(checkPallindromeForAllDares(date));
+console.log(getNextDate(date));
